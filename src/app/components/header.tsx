35fbe33b6082f93e5bd523867d458dd9c692c75f
CoolGuyOn <williambang24@icloud.com>
2024-06-14 18:16:10 +0100

    Create booking form

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -21,6 +21,12 @@ const Header = () => {
             >
               Home
             </Link>
+            <Link
+              href="/booking"
+              className="text-accent dark:accent hover:bg-neutral font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+            >
+              Make A Booking
+            </Link>
             {isLoaded && user ? (
               <SignOutButton>
                 <button className="text-accent dark:accent hover:bg-neutral font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
